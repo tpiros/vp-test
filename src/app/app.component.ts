a09b36b4c8ab2e3d10ce1504aa6e39c24d39c70d
Tamas Piros <tpiros@tpiros-MacBook-Air.local>
2024-06-20 15:18:25 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
+import cloudinary from 'cloudinary-video-player';
+import 'cloudinary-video-player/cld-video-player.min.css';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'vp-test';
+export class AppComponent implements OnInit {
+  @ViewChild('player', { static: true }) video!: ElementRef;
+  player!: cloudinary.VideoPlayer;
+  ngOnInit() {
+    this.initializePlayer();
+  }
+
+  initializePlayer(): void {
+    const options = {
+      controls: true,
+      cloudName: 'tamas-demo',
+    };
+
+    this.player = cloudinary.videoPlayer(this.video.nativeElement, options);
+    this.player.source('harry-potter-trailer');
+  }
 }
